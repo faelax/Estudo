66d3726627cc6f0c81bf3997d787fadb7f2dde51
Raphael Alexandre de Santana Monteiro <40326175+faelax@users.noreply.github.com>
2018-06-16 20:47:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
 
 
-<<<<<<< HEAD
+
 
 	echo"KOE RAPAZIADA lol";
 	echo" KOE NEGADA";
-=======
-	echo"chama o sindico TIM MAIA";
-	echo"KOE RAPAZIADA";
->>>>>>> cf6f9b073064791aff6de64741b7514587eed53e
 
 
 
